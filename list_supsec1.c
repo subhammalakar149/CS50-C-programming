c1010f11bbf79571b539fdeab60c34b5dd102408
yahngs <subham.malakar149@gmail.com>
2025-01-30 19:03:55 +0530

    Created list_supsec1.c

---
+++ list_supsec1.c
@@ -0,0 +1,73 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+}
+node;
+
+#define LIST_SIZE 2
+
+void unload(node *list);
+void visualize(node *list);
+
+int main(void)
+{
+    node *list = NULL;
+
+    // Add items to list
+    for (int i = 0; i < LIST_SIZE; i++)
+    {
+        string phrase = get_string("Enter a new phrase: ");
+
+        // TODO: add phrase to new node in list
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->phrase = phrase;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
+
+        // Visualize list after adding a node.
+        visualize(list);
+    }
+
+    unload(list);
+}
+
+void unload(node *list)
+{
+    // TODO: Free all allocated nodes
+    node *ptr = list;
+
+    while(ptr != NULL)
+    {
+        ptr = list->next;
+        free(list);
+        list = ptr;
+    }
+    //return true;
+
+}
+
+void visualize(node *list)
+{
+    printf("\n+-- List Visualizer --+\n\n");
+    while (list != NULL)
+    {
+        printf("Location %p\n", list);
+        printf("Phrase: \"%s\"\n", list->phrase);
+        printf("Next: %p\n\n", list->next);
+        list = list->next;
+    }
+    printf("+---------------------+\n\n");
+}
